b55f4294ef5462f28e00e2199ad38190cb448093
muddu2602 <abhishek.muddu26@gmail.com>
2018-01-28 18:58:49 +0530

    base files for the project

---
+++ js/app.js
@@ -0,0 +1,53 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBsSu78Z2xWy2neUIH0eD77KA0px7i1iBM",
+    authDomain: "ityukta-2k18.firebaseapp.com",
+    databaseURL: "https://ityukta-2k18.firebaseio.com",
+    projectId: "ityukta-2k18",
+    storageBucket: "",
+    messagingSenderId: "902683637359"
+  };
+  firebase.initializeApp(config);
+  //reference messages collection
+  var user = firebase.database().ref('users');
+
+
+  //Listen for Form Details
+document.getElementById('registerFormItyukta').addEventListener('submit' , submitForm);
+//Submit Form
+function submitForm(e){
+    e.preventDefault();
+    // get values
+    var name = getInputVal('name');
+    var phone = getInputVal('phoneNo');
+    var email = getInputVal('email');
+    
+    saveMessage(name , email , phone);
+
+    //Show Alert
+    document.querySelector('.alert').style.display = 'block';
+
+    setTimeout(function(){
+        document.querySelector('.alert').style.display = 'none';
+        window.location="/";
+    },3000);
+
+    
+    
+}
+//Function to get Form Values
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+
+
+//Save messges to database
+function saveMessage(name , email , phone){
+    var newMessageRef = user.push();
+    newMessageRef.set({
+        name:name,
+        email:email,
+        phone:phone
+    });
+}
